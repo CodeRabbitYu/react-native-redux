2c575e394999789e54fde1f4502d7d1f8623acc1
Rabbit <515236489@qq.com>
2017-08-09 22:47:08 +0800

    update

--- index.ios.js
+++ index.ios.js
@@ -2,16 +2,6 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  * @flow    
- * 大聪明是我
- * 我是大聪明
- * 我怎么能这么可怜 🤕
- * 我好可怜
- * 唉 还得早起
- * 傻逼傻逼大傻逼
- * 想静静
- * 凤梨酥
- * 6666
- * 都给你卖了 买肉吃
  */
 
 global.__APP__ = true;
